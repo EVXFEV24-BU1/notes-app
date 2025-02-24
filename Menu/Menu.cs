85aa30bf4a45b052b2ed0928ffbd6289960c76b2
EnanderW <williamenander@gmail.com>
2025-02-24 14:34:04 +0100

    update Menu class

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -26,21 +26,52 @@ public abstract class Menu
             {
                 // Kolla om typen (klassen) är ett kommando
                 // genom att se om typen ärver av 'Command'
-                if (type.IsSubclassOf(typeof(Command)))
+                if (!type.IsSubclassOf(typeof(Command)))
                 {
-                    // Skapa en instans av kommando klassen genom reflection
-                    // och skicka in dependency providern som argument
-                    Command command = (Command)Activator.CreateInstance(type, dependencyProvider);
+                    continue;
+                }
 
+                // Skapa en instans av kommando klassen genom reflection
+                // och skicka in dependency providern som argument
+                Command command = (Command)Activator.CreateInstance(type, dependencyProvider);
+
+                // Hämta attributen som ligger på kommandot, om det finns en attribut
+                MenuCommandAttribute? attribute = type.GetCustomAttribute<MenuCommandAttribute>();
+                if (attribute == null)
+                {
+                    // Registrera kommandot (automatiskt)
+                    RegisterCommand(command);
+                }
+                // Kolla om kommandot skall finnas i denna meny
+                else if (this.GetType().Equals(attribute.MenuType))
+                {
                     // Registrera kommandot (automatiskt)
                     RegisterCommand(command);
                 }
             }
         }
+
+        // Man kan även implementera allt ovanför med LINQ, det blir lite mindre kod
+        /*var commands = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes()) // Hämta alla typer i alla program till en lista
+            .Where(commandType => commandType.IsSubclassOf(typeof(Command))) // Filtrera ut icke-kommando klasser
+            .Where(commandType =>
+            {
+                // Välj ut de kommandon som ska tillhöra menyn
+                MenuCommandAttribute? attribute = commandType.GetCustomAttribute<MenuCommandAttribute>();
+                return attribute == null || this.GetType().Equals(attribute.MenuType);
+            })
+            .Select(commandType => (Command)Activator.CreateInstance(commandType, dependencyProvider)); // Skapa instanser av kommando klasserna
+
+        // Registrera alla kommandon
+        foreach (Command command in commands)
+        {
+            RegisterCommand(command);
+        }*/
     }
 
-    // Registrera ett kommandon för menyn, detta görs i menyns constructor (se LoginMenu för ett exempel)
-    public void RegisterCommand(Command command)
+    // Registrera ett kommandon för menyn
+    private void RegisterCommand(Command command)
     {
         this.commands.Add(command);
     }
