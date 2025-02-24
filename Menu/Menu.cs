5e037f623f23c1a86d7792e760883175cbec4746
EnanderW <williamenander@gmail.com>
2025-02-24 13:26:27 +0100

    add comments for reflection

--- Menu/Menu.cs
+++ Menu/Menu.cs
@@ -8,20 +8,31 @@ public abstract class Menu
 
     public Menu(DependencyProvider dependencyProvider)
     {
+        // AppDomain och Assembly är representationer av program.
+        // 'notes-app' är ett av dem (för det finns fler som inbyggda C# saker exempelvis)
         AppDomain domain = AppDomain.CurrentDomain;
         Assembly[] assemblies = domain.GetAssemblies();
 
+        // Vi behöver egentligen bara vår egen assembly (program) men vi vet inte vilken den är
+        // så vi loopar igenom alla
         foreach (Assembly assembly in assemblies)
         {
+            // Hämta alla klasser, interfaces och andra typer (Type) i vårt program
+            // Typerna är exempelvis: Command, Note, User, IUserService och så vidare
             Type[] types = assembly.GetTypes();
 
+            // Loopa igenom alla typer
             foreach (Type type in types)
             {
+                // Kolla om typen (klassen) är ett kommando
+                // genom att se om typen ärver av 'Command'
                 if (type.IsSubclassOf(typeof(Command)))
                 {
-                    Console.WriteLine(type.FullName);
+                    // Skapa en instans av kommando klassen genom reflection
+                    // och skicka in dependency providern som argument
                     Command command = (Command)Activator.CreateInstance(type, dependencyProvider);
 
+                    // Registrera kommandot (automatiskt)
                     RegisterCommand(command);
                 }
             }
